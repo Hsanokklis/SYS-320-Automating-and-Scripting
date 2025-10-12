6a9c2de583c30f9a1d0baeda4d31d907bcb275be
hsanokklis <hsanokklis@gmail.com>
2025-10-12 19:48:03 -0400

    windows apache logs

---
+++ week4_5/apache-logs.ps1
@@ -0,0 +1,28 @@
+﻿# Function called Apache-Logs that takes 3 inputs, page visited, http code returned, and name of web browser
+
+function Get-ApacheLogs {
+    param(
+        $page_visited_,
+        $http_code,
+        $browser_name
+    )
+
+    #Store log content from access.log
+    $logContent = Get-Content C:\xampp\apache\logs\access.log
+
+    #Filter Logs
+    $filteredLogs = $logContent | Where-Object {
+        $_ -like "*$page_visited*" -and
+        $_ -like "*$http_code*" -and
+        $_ -like "*$browser_name*"
+    }
+
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+    $ips = $regex.Matches($filteredLogs)
+
+    #Extract and return IP address
+    $ips.Value
+}
+
+
+
---
+++ week4_5/last_five_log_error.ps1
@@ -0,0 +1,5 @@
+﻿# From every .log file in the directory, only get the logs that contains the word 'error'
+$A = Get-Content C:\xampp\apache\logs\*.log | Select-String -Pattern 'error'
+
+#Display last 5 elements for result array
+$A[-5..-1]
