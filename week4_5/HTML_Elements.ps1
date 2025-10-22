584397ab013b927fda0e88fd4bfc6777e6bd39c1
hsanokklis <hsanokklis@gmail.com>
2025-10-22 18:48:57 -0400

    Web Scraping script

---
+++ week4_5/HTML_Elements.ps1
@@ -0,0 +1,8 @@
+﻿$scraped_page = Invoke-WebRequest -Uri http://10.0.17.37/ToBeScraped.html
+
+#Get a count of the links in the page
+$linkCount = $scraped_page.Links
+
+Write-Host "Number of links on the page: $linkCount"
+ 
+
