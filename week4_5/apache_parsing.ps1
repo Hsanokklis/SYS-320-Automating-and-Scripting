f8e51e18c1a76970c497444e3b7166e1ef5ab377
hsanokklis <hsanokklis@gmail.com>
2025-11-01 18:41:06 -0400

    apache parsing

---
+++ week4_5/apache_parsing.ps1
@@ -0,0 +1,32 @@
+﻿# This function reads Apache web server log files, parses each log entry into structured data, filters for internal
+# network traffic, and displays the results in a formatted table. 
+
+function ApacheLogs(){
+    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+    #creates an empty array to store parsed log records
+    $tableRecords = @()
+    
+    for($i=0; $i -lt $logsNotFormatted.Length; $i++) {
+        # split a string into words 
+        $words = $logsNotFormatted[$i].Split(" ");
+        
+        # parse each word into a structred pscustomobject and add it to the table
+        $tableRecords += [pscustomobject]@{
+            "IP" = $words[0];
+            "Time" = $words[3].Trim('[');
+            "Method" = $words[5].Trim('"');
+            "Page" = $words[6];
+            "Protocol" = $words[7].Trim('"');
+            "Response" = $words[8];
+            "Referrer" = $words[10];
+            "Client" = $words[11..($words.Count -1)] -join ' '
+        }
+    }
+    
+    #Filters the records to only return entires where the IP starts with "10"
+    return $tableRecords | Where-Object {$_.IP -ilike "10.*"}
+}
+
+#Calls the function and display result if we were to run it from this script. 
+#$tableRecords = ApacheLogs
+#$tableRecords | Format-Table -AutoSize -Wrap
---
+++ week4_5/main_parsing.ps1
@@ -0,0 +1,8 @@
+﻿# Dot source the function
+. C:\Users\champuser\SYS-320-Automating-and-Scripting\week4_5\apache_parsing.ps1
+
+
+Write-Host = "Parsing Apache Logs Function Output:"
+ApacheLogs | Format-Table -AutoSize -Wrap
+
+
